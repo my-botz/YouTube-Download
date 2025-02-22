6c4253e83defc766e01d803e96e9b1c879216e1b
my-botz <bdata2666@gmail.com>
2025-02-22 23:13:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ from datetime import timedelta
 # ------ הגדרות קבועות ------
 API_ID = int(os.environ.get("API_ID"))
 API_HASH = os.environ.get("API_HASH")
-BOT_TOKEN = os.environ.get("BOT_TOKEN"))
+BOT_TOKEN = os.environ.get("BOT_TOKEN")
 COOKIES_FILE = "cookies.txt"
 
 # ------ וידוא קובץ קוקיז ------
